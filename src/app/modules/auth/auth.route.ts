c0c3b076a6df42a1456cd3c6fc990ac70e03ca2d
unknown <mdomarfaruk149518@gmail.com>
2023-09-28 20:46:40 +0600

    [Don]faculty

--- src/app/modules/auth/auth.route.ts
+++ src/app/modules/auth/auth.route.ts
@@ -2,6 +2,8 @@ import express from 'express';
 import { AuthenticationController } from './auth.controller';
 import validateRequest from '../../middlewares/validateRequest';
 import { AuthValidation } from './auth.validation';
+import auth from '../../middlewares/auth';
+import { ENUM_USER_ROLE } from '../../../enums/user';
 
 const router = express.Router();
 
@@ -17,6 +19,12 @@ router.post(
 );
 router.post(
   '/change-password',
+  auth(
+    ENUM_USER_ROLE.SUPER_ADMIN,
+    ENUM_USER_ROLE.ADMIN,
+    ENUM_USER_ROLE.STUDENT,
+    ENUM_USER_ROLE.FACULTY
+  ),
   validateRequest(AuthValidation.changePasswordZodSchema),
   AuthenticationController.changePassword
 );
